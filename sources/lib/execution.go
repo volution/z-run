1094f0a5cfe5be8f940e219662ce4d173386a38c
Ciprian Dorin Craciun <ciprian@volution.ro>
2019-12-14 12:22:18 +0200

    [fix]  Emit an error if `<print>` scriptlet is given extra arguments

--- sources/lib/execution.go
+++ sources/lib/execution.go
@@ -16,6 +16,31 @@ func prepareExecution (_libraryUrl string, _interpreter string, _scriptlet *Scri
 	
 	var _interpreterExecutable string
 	var _interpreterArguments []string = make ([]string, 0, len (_context.cleanArguments) + 16)
+	var _interpreterAllowsArguments = false
+	
+	if _interpreter == "" {
+		_interpreter = _scriptlet.Interpreter
+	}
+	
+	
+	switch _interpreter {
+		
+		case "<shell>" :
+			_interpreterAllowsArguments = true
+		
+		case "<print>" :
+			_interpreterAllowsArguments = false
+		
+		case "<menu>" :
+			_interpreterAllowsArguments = false
+		
+		default :
+			return nil, nil, errorf (0x0873f2db, "unknown scriptlet interpreter `%s` for `%s`", _interpreter, _scriptlet.Label)
+	}
+	
+	if _includeArguments && (len (_context.cleanArguments) > 0) && !_interpreterAllowsArguments {
+		return nil, nil, errorf (0x4ef9e048, "unexpected arguments")
+	}
 	
 	var _interpreterScriptInput int
 	var _interpreterScriptOutput *os.File
@@ -29,10 +54,6 @@ func prepareExecution (_libraryUrl string, _interpreter string, _scriptlet *Scri
 	
 	_interpreterScriptBuffer := bytes.NewBuffer (nil)
 	_interpreterScriptBuffer.Grow (128 * 1024)
-	
-	if _interpreter == "" {
-		_interpreter = _scriptlet.Interpreter
-	}
 	
 	switch _interpreter {
 		
@@ -79,9 +100,7 @@ exec %d<&-
 			_interpreterScriptBuffer.WriteString (_scriptlet.Body)
 		
 		default :
-			syscall.Close (_interpreterScriptInput)
-			_interpreterScriptOutput.Close ()
-			return nil, nil, errorf (0x0873f2db, "unknown scriptlet interpreter `%s` for `%s`", _interpreter, _scriptlet.Label)
+			panic (0xe95f68a0)
 	}
 	
 //	logf ('d', 0xedfcf88b, "\n----------\n%s----------\n", _interpreterScriptBuffer.Bytes ())
