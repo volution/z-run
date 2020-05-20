cf6e20bc321fca8a8c82d97021490a0bdb125a92
Ciprian Dorin Craciun <ciprian@volution.ro>
2020-05-21 00:30:47 +0300

    [tools]  Temporary code duplication to make it compile on OSX.

--- sources/lib/execution.go
+++ sources/lib/execution.go
@@ -7,11 +7,8 @@ import "bytes"
 import "fmt"
 import "os"
 import "os/exec"
-import "path"
 import "strings"
 import "syscall"
-
-import "golang.org/x/sys/unix"
 
 
 
@@ -51,40 +48,13 @@ func prepareExecution (_libraryUrl string, _libraryFingerprint string, _interpre
 	
 	var _interpreterScriptInput int
 	var _interpreterScriptOutput *os.File
-	var _interpreterScriptDescriptors [2]int
 	var _interpreterScriptUnused = false
 	
-	if len (_scriptlet.Body) < (1 * 1024 * 1024 - 128 * 1024) {
-		if _error := syscall.Pipe (_interpreterScriptDescriptors[:]); _error == nil {
-			if _, _error := unix.FcntlInt (uintptr (_interpreterScriptDescriptors[1]), unix.F_SETPIPE_SZ, 1 * 1024 * 1024); _error != nil {
-				logf ('w', 0x4d3414c8, "failed increasing pipe buffer size;  ignoring!")
-			}
-			_interpreterScriptInput = _interpreterScriptDescriptors[0]
-			_interpreterScriptOutput = os.NewFile (uintptr (_interpreterScriptDescriptors[1]), "")
-		} else {
-			return nil, nil, errorw (0xece645ff, _error)
-		}
+	if _interpreterScriptInput_0, _interpreterScriptOutput_0, _error := createPipe (len (_scriptlet.Body), _context.cacheRoot); _error == nil {
+		_interpreterScriptInput = _interpreterScriptInput_0
+		_interpreterScriptOutput = _interpreterScriptOutput_0
 	} else {
-		if _context.cacheRoot == "" {
-			// FIXME:  We should make sure that the cache path is never empty!
-			panic (0xd6f17610)
-		}
-		_temporaryPath := path.Join (_context.cacheRoot, generateRandomToken () + ".scriptlet")
-		if _descriptor, _error := syscall.Open (_temporaryPath, syscall.O_CREAT | syscall.O_EXCL | syscall.O_WRONLY, 0600); _error == nil {
-			_interpreterScriptOutput = os.NewFile (uintptr (_descriptor), "")
-		} else {
-			return nil, nil, errorw (0x2b19feaa, _error)
-		}
-		if _descriptor, _error := syscall.Open (_temporaryPath, syscall.O_RDONLY, 0600); _error == nil {
-			_interpreterScriptInput = _descriptor
-		} else {
-			// FIXME:  Here we leak the first descriptor!
-			return nil, nil, errorw (0x694ce572, _error)
-		}
-		if _error := syscall.Unlink (_temporaryPath); _error != nil {
-			// FIXME:  Here we leak both descriptors!
-			return nil, nil, errorw (0xc5afd6fd, _error)
-		}
+		return nil, nil, _error
 	}
 	
 	_interpreterScriptBuffer := bytes.NewBuffer (nil)
---
+++ sources/lib/tools_others.go
@@ -0,0 +1,56 @@
+
+// +build !linux
+
+
+package zrun
+
+
+import "os"
+import "path"
+import "syscall"
+
+
+
+
+// FIXME:  Merge with Linux variant!
+
+func createPipe (_size int, _cacheRoot string) (int, *os.File, *Error) {
+	
+	var _interpreterScriptInput int
+	var _interpreterScriptOutput *os.File
+	var _interpreterScriptDescriptors [2]int
+	
+	if _size < (16 * 1024 - 2 * 1024) {
+		if _error := syscall.Pipe (_interpreterScriptDescriptors[:]); _error == nil {
+			_interpreterScriptInput = _interpreterScriptDescriptors[0]
+			_interpreterScriptOutput = os.NewFile (uintptr (_interpreterScriptDescriptors[1]), "")
+		} else {
+			return -1, nil, errorw (0xece645ff, _error)
+		}
+	} else {
+		if _cacheRoot == "" {
+			// FIXME:  We should make sure that the cache path is never empty!
+			panic (0xd6f17610)
+		}
+		_temporaryPath := path.Join (_cacheRoot, generateRandomToken () + ".scriptlet")
+		if _descriptor, _error := syscall.Open (_temporaryPath, syscall.O_CREAT | syscall.O_EXCL | syscall.O_WRONLY, 0600); _error == nil {
+			_interpreterScriptOutput = os.NewFile (uintptr (_descriptor), "")
+		} else {
+			return -1, nil, errorw (0x2b19feaa, _error)
+		}
+		if _descriptor, _error := syscall.Open (_temporaryPath, syscall.O_RDONLY, 0600); _error == nil {
+			_interpreterScriptInput = _descriptor
+		} else {
+			// FIXME:  Here we leak the first descriptor!
+			return -1, nil, errorw (0x694ce572, _error)
+		}
+		if _error := syscall.Unlink (_temporaryPath); _error != nil {
+			// FIXME:  Here we leak both descriptors!
+			return -1, nil, errorw (0xc5afd6fd, _error)
+		}
+	}
+	
+	return _interpreterScriptInput, _interpreterScriptOutput, nil
+}
+
+
