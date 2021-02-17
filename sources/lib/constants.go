8c92c5bfe5143ec5d644c6394ee12cf21957d892
Ciprian Dorin Craciun <ciprian@volution.ro>
2021-02-17 18:53:16 +0200

    [embedded]  Use an internal `embedded` package that uses Go 1.16 feature of file embedding.

--- sources/lib/constants.go
+++ sources/lib/constants.go
@@ -3,7 +3,10 @@
 package zrun
 
 
+import embedded "github.com/cipriancraciun/z-run/embedded"
+
 import "runtime"
+import "strings"
 
 
 
@@ -14,11 +17,9 @@ var BUILD_TARGET_OS string = runtime.GOOS
 var BUILD_COMPILER_TYPE string = runtime.Compiler
 var BUILD_COMPILER_VERSION string = runtime.Version ()
 
-var BUILD_VERSION string = "{unknown-version}"
-var BUILD_NUMBER string = "{unknown-build}"
-
-var BUILD_COMMIT string = "{unknown-commit}"
-var BUILD_TIMESTAMP string = "{unknown-timestamp}"
-var BUILD_SOURCES_MD5 string = "{unknown-sources-md5}"
+var BUILD_VERSION string = strings.Trim (embedded.BuildVersion, "\n")
+var BUILD_NUMBER string = strings.Trim (embedded.BuildNumber, "\n")
+var BUILD_TIMESTAMP string = strings.Trim (embedded.BuildTimestamp, "\n")
+var BUILD_SOURCES_MD5 string = strings.Trim (embedded.BuildSourcesMd5, "\n")
 var BUILD_GIT_HASH string = "${unknown-git-hash}"
 
--- sources/lib/embedded.go
+++ sources/lib/embedded.go
@@ -1,1 +1,19 @@
-../embedded.go
+
+
+package zrun
+
+
+import embedded "github.com/cipriancraciun/z-run/embedded"
+
+
+
+
+var embeddedGoProlog string = embedded.GoProlog
+var embeddedPython3Prolog string = embedded.Python3Prolog
+var embeddedBashProlog0 string = embedded.BashProlog0
+var embeddedBashProlog string = embedded.BashProlog
+var embeddedBashShellRc string = embedded.BashShellRc
+var embeddedBashShellFunctions string = embedded.BashShellFunctions
+
+var embeddedManualTxt string = embedded.ManualTxt
+
