291d377af34f4ddade1570f5d5013b83fcca1ab1
Ciprian Dorin Craciun <ciprian@volution.ro>
2022-01-19 17:17:02 +0200

    [embedded]  Add readme support into executable.

--- sources/embedded/manual.go
+++ sources/embedded/manual.go
@@ -8,6 +8,13 @@ import _ "embed"
 
 
 
+//go:embed manual/readme.txt
+var ReadmeTxt string
+
+//go:embed manual/readme.html
+var ReadmeHtml string
+
+
 //go:embed manual/z-run--help.txt
 var ZrunHelpTxt string
 
@@ -19,4 +26,11 @@ var ZrunManualHtml string
 
 //go:embed manual/z-run--manual.man
 var ZrunManualMan string
+
+
+//go:embed manual/help--header.txt
+var HelpHeader string
+
+//go:embed manual/help--footer.txt
+var HelpFooter string
 
